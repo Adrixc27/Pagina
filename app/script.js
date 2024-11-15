fbe0e54db1ad902da05f0fe95a374432f33aeb49
Adrixc27 <elcreeper97oficcial@gmail.com>
2024-11-15 01:19:36 -0600

    Update script.js

--- app/script.js
+++ app/script.js
@@ -10,24 +10,27 @@ window.addEventListener('beforeinstallprompt', (e) => {
     installButton.style.display = 'block'; // Muestra el botón
 
     installButton.addEventListener('click', () => {
-        // Muestra el prompt de instalación
-        deferredPrompt.prompt();
+        // Si deferredPrompt no es nulo, muestra el prompt de instalación
+        if (deferredPrompt) {
+            deferredPrompt.prompt(); // Muestra el diálogo de instalación
 
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('El usuario aceptó la instalación');
-            } else {
-                console.log('El usuario rechazó la instalación');
-            }
-            deferredPrompt = null; // Limpia el prompt
-        });
+            // Responde a la acción del usuario
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('El usuario aceptó la instalación');
+                } else {
+                    console.log('El usuario rechazó la instalación');
+                }
+                deferredPrompt = null; // Limpia el prompt después de que el usuario haga su elección
+            });
+        }
     });
 });
 
-// Registra el service worker
+// Registra el Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js').then((registration) => {
+        navigator.serviceWorker.register('/sw.js').then((registration) => {
             console.log('Service Worker registrado con éxito:', registration);
         }).catch((error) => {
             console.log('Registro de Service Worker fallido:', error);
